b0ed7aac68b98abc11193589ca8e86083dc10582
SUMEET DWIVEDI <111240054+motivated-coder@users.noreply.github.com>
2023-01-03 00:12:08 +0530

    Revert "Created Controller to handle requests from Rest API endpoints."

--- order-service/order-domain/order-application-service/src/main/java/com/foodie/odo/appservice/dto/TrackOrderResponse.java
+++
@@ -1,5 +0,0 @@
-package com.foodie.odo.appservice.dto;
-
-public class TrackOrderResponse {
-
-}
--- order-service/order-domain/order-domain-core/src/main/java/com/foodie/odo/core/entity/Order.java
+++
@@ -1,6 +0,0 @@
-package com.foodie.odo.core.entity;
-
-import com.foodie.common.entity.AggregateRoot;
-
-public class Order extends AggregateRoot {
-}
